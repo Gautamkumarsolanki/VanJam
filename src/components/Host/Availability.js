8e8691d8764aa797af92f5ec5fd92207c9125202
Gautamkumarsolanki <42197609+Gautamkumarsolanki@users.noreply.github.com>
2023-08-19 11:21:01 +0530

    minor changes

--- src/components/Host/Availability.js
+++ src/components/Host/Availability.js
@@ -1,21 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useOutletContext, useParams } from 'react-router-dom';
 import './togglestyle.css';
+import { DateRangePicker } from 'react-date-range';
+import 'react-date-range/dist/styles.css';
+import 'react-date-range/dist/theme/default.css';
+import { doc, getFirestore, setDoc } from 'firebase/firestore';
+import { useEffect } from 'react';
 
+const db=getFirestore();
 export default function Availability() {
+    const { data } = useOutletContext();
     const { id } = useParams();
-    const { data, setVanDetail } = useOutletContext();
+    const [current, setCurrent] = useState({ startDate: new Date(), endDate: new Date(), key: "selection", color: 'black' });
+    const [available, setAvailable] = useState(data.available === "true"? true : false)
+    const onChangeHandler = async (e) => {
+        if (e.target.checked) {
+            setAvailable(true);
+            await setDoc(doc(db,"BS0nrs2L1yX2JlcUeDgRVKg1xhI2",id),{
+                available:"true"
+            },{merge:true});
+        }
+        else {
+            setAvailable(false)
+            await setDoc(doc(db,"BS0nrs2L1yX2JlcUeDgRVKg1xhI2",id),{
+                available:new Date().getFullYear()+"-"+(new Date().getMonth()+1<10?"0"+(new Date().getMonth()+1):new Date().getMonth()+1)+"-"+new Date().getDate()
+            },{merge:true});
+        }
+    }
+    useEffect(()=>{
+        if(data.available!=="true"){
+            setCurrent({startDate: new Date(), endDate: new Date(data.available), key: "selection", color: 'black'})
+        }
+    },[data.available])
     return (
 
         <div className="flex flex-col md:flex-row flex-wrap justify-around">
-            <label for="foobar1" className="flex flex-col flex-wrap items-center cursor-pointer mb-4 md:mb-0">
+            <label htmlFor="foobar1" className="flex flex-col flex-wrap items-center cursor-pointer mb-4 md:mb-0">
                 <span className="font-semibold my-4 block">Toggle to set Availability</span>
                 <div className="relative">
-                    <input id="foobar1" type="checkbox" className="hidden" />
-                    <div className="toggle__line w-12 h-6 bg-gray-200 rounded-full shadow-inner"></div>
-                    <div className="toggle__dot absolute w-5 h-5 bg-white rounded-full shadow inset-y-0 left-0"></div>
+                    <input onChange={onChangeHandler} checked={available} id="foobar1" type="checkbox" className="hidden" />
+                    <div className="toggle__line w-[2.9rem] h-6 bg-gray-200 rounded-full shadow-inner"></div>
+                    <div className="toggle__dot absolute left-[1px] top-[2px] w-5 h-5 bg-white rounded-full shadow inset-y-0"></div>
                 </div>
-            </label>
+                <p className='font-semibold text-xl my-4'>{available ? "Available" : `Not Available till ${current.endDate}`}</p>
+                {!available &&
+                    <DateRangePicker minDate={new Date()} onChange={async(item)=>{
+                        setCurrent(item.selection)
+                        await setDoc(doc(db,"BS0nrs2L1yX2JlcUeDgRVKg1xhI2",id),{
+                            available:current.endDate.getFullYear()+"-"+(current.endDate.getMonth()+1<10?"0"+(current.endDate.getMonth()+1):current.endDate.getMonth()+1)+"-"+current.endDate.getDate()
+                        },{merge:true})
+                    }} ranges={[current]} />
+                }
+                
+                </label>
         </div>
     )
 }
--- src/components/Host/Photos.js
+++ src/components/Host/Photos.js
@@ -1,19 +1,63 @@
-import React from 'react'
+import { CloudArrowUpIcon } from '@heroicons/react/24/outline';
+import React, { useRef, useState } from 'react'
 import { useOutletContext } from 'react-router-dom';
 
 export default function Photos() {
 	const { data } = useOutletContext();
+	const reff = useRef(null);
+	const [selectedFiles, setSelectedFiles] = useState([]);
+	const [preview, setPreview] = useState([]);
+	const handleClick = () => {
+		reff.current?.click();
+	}
+	const fileChangeHandler = (e) => {
+		let files = [];
+		let pre = [];
 
+		for (const file of e.target.files) {
+			pre.push(URL.createObjectURL(file));
+			files.push(file);
+		}
+		setSelectedFiles(files);
+		setPreview(pre)
+
+	}
 	return (
 		<>
-			<div className='px-24 flex space-x-6'>
-				{
-					data.images.map((ele, i) => {
-						return (
-							<img key={i} className='h-36 w-36' alt='' src={ele} />
+			<div className='px-24 flex flex-col space-x-6'>
+				<div className='flex space-x-6'>
+					{
+						data.images.map((ele, i) => {
+							return (
+								<img key={i} className='h-36 w-36' alt='' src={ele} />
+							)
+						})
+					}
+					<div onClick={handleClick} className='pt-10 px-4 flex flex-col items-center bg-violet-100 text-violet-500 rounded-lg hover:bg-violet-100 cursor-pointer'>
+						<CloudArrowUpIcon className='w-6 h-6' />
+						<span>Choose images to upload</span>
+						<input multiple type="file" onChange={fileChangeHandler} ref={reff} className="hidden" />
+					</div>
+				</div>
+				<div>
+					{
+						selectedFiles.length > 0 && (
+							<div className='p-4 mt-4 bg-violet-50 overflow-hidden text-ellipsis'>
+								<p>Selected Files:</p>
+								<div className='flex space-x-4'>
+									{
+										preview.map((file, i) => {
+											return (
+												<img key={i} alt='' className='h-40 w-40' src={file} />
+											)
+										})
+									}
+								</div>
+							</div>
 						)
-					})
-				}
+					}
+				</div>
+
 			</div>
 		</>
 	)
